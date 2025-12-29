4e3724a2c27435979a876ddd954507568ab9e6a7
purellarishika <purellarishika@gmail.com>
2025-12-29 15:56:24 +0530

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,9 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
+import { config } from "dotenv";
+config();
+
 
 const app = express();
 const httpServer = createServer(app);
